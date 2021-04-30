ba494eda04e25062bb1b451d7aeb980e45d7824c
pocota5260 <t.yamaguchi5260@gmail.com>
2021-05-01 00:13:37 +0900

    GETとPOSTのテスト

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
- 
-// デフォルトルーティング
+var resObj = {
+    title: 'Sample NodeExpTest',
+    message: 'Hello World!',
+    name: "",
+    age: "",
+    sports: ""
+};
+
 router.get('/', function (request, response) {
-    response.render('index', { title: 'Sample NodeExpTest', message: 'Hello World!' });
+    // response.render('index', { title: 'Sample NodeExpTest', message: 'Hello World!' });
+    const query = request.query;
+    resObj.name = query.name;
+    resObj.age = query.age;
+    response.render('index', resObj);
+});
+
+router.post("/", function (request, response) {
+    resObj.sports = request.body.sports;
+    response.render('index', resObj);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// POSTのクエリーを良い感じに処理する
+app.use(express.urlencoded({extended: true}));
+
 // テンプレートエンジンを EJS に設定
 app.set('views', './views');
 app.set('view engine', 'ejs');
